e2f8407f40547cfba87d72310ab64c26338ca1a8
Abdalhaqq Muhammad Saih <abdalhaqqm@gmail.com>
2024-05-29 08:58:56 +0700

    feat: initial commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Materials',
+    'version': '0.0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'contacts'],
+    'summary': 'Materials Management',
+    'category': 'Inventory',
+}
