2e69c08b7bd929649289445e9c67e1bc6280c37f
Abdalhaqq Muhammad Saih <abdalhaqqm@gmail.com>
2024-05-29 13:24:55 +0700

    chore: update manifest file with necessary changes

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,14 @@
 {
     'name': 'Materials',
     'version': '0.0.1',
+    'author': 'Abdalhaqq Muhammad Saih',
+    'website': 'https://github.com/dalhaqq/odoo-materials-addon',
     'application': True,
     'installable': True,
     'depends': ['base', 'contacts'],
     'summary': 'Materials Management',
     'category': 'Inventory',
+    'data': [
+            'security/ir.model.access.csv'
+    ]
 }
